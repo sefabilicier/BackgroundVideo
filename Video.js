08b1754578e1675a2d63ec9e9fe7a9d90ab20efa
sefabilicier <sefabilicier@gmail.com>
2023-10-25 18:42:44 +0300

    first commit

---
+++ Video.js
@@ -0,0 +1,43 @@
+const btn = document.querySelector('.switch-btn');
+const video = document.querySelector('.video-container');
+
+btn.addEventListener("click", function () {
+    if (!btn.classList.contains("slide")) {
+        btn.classList.add("slide");
+        video.pause();
+    } else {
+        btn.classList.remove("slide");
+        video.play();
+    }
+});
+
+
+//preloader
+const preloader = document.querySelector('.preloader')
+
+window.addEventListener("load", function () {
+    setTimeout(function () {
+      preloader.classList.add("hide-preloader")  
+    },3000);
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
